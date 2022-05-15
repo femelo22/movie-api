48c40efc64ffd3096cc69d4a250182e75943c23b
femelo22 <luiz123jfmg@gmail.com>
2022-05-15 20:49:43 -0300

    feat: create category controller

---
+++ src/controller/CreateCategoryController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import { CreateCategorySerivce } from "../services/CreateCategoryService";
+
+export class CreateCategoryController {
+    async handle(request: Request, response: Response) {
+        const {name, description } = request.body;
+
+        const service = new CreateCategorySerivce();
+
+        const result = service.execute({ name, description });
+
+        if(result instanceof Error) {
+            return response.status(400).json(result.message);
+        }
+
+        return response.json(result);
+    }
+}
